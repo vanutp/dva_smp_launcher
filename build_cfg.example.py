8aef5970d2af5e6bb80f1d3a3434004079b893ec
Ivan Filipenkov <hello@vanutp.dev>
2024-06-30 01:17:51 +0200

    Get bot username from tgauth server

--- build_cfg.example.py
+++ build_cfg.example.py
@@ -3,7 +3,6 @@ LAUNCHER_NAME = "dva_smp_launcher"
 DATA_DIR_NAME = "dvasmp"
 
 TGAUTH_BASE = "http://127.0.0.1:8080"
-TGAUTH_BOT_NAME = "dva_smp_auth_bot"
 
 ELYBY_CLIENT_ID = "dvasmp1"
 ELYBY_CLIENT_SECRET = "very-secret"
